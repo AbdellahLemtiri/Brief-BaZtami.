bea2ca6c881bc65a3ccd3a87fcf9b93efd1c6553
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-10-29 17:04:10 +0100

    ajoute button d'afficher la formulaire

---
+++ script.js
@@ -0,0 +1,5 @@
+var myvar= document.getElementById('ajouterBtn');
+var my2var = document.getElementById('formulaire');
+myvar.addEventListener('click', function(){
+    my2var.classList.remove('d-none');
+});
